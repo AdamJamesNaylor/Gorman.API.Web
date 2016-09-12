116c9a747d85dfb85154d45c490d9f619432234a
Adam Naylor <adamnaylor@gmail.com>
2016-09-12 17:21:06 +0100

    Initial commit

---
+++ Gorman.API.Web/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Gorman.API.Web.Controllers
+{
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Framework;
+    using Framework.Services;
+    using Framework.Validators;
+
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public async Task<ActionResult> Index() {
+            var endpoints = await Endpoints.Get("http://localhost:9999", new ResponseValidator());
+            var service = new FullGraphService(endpoints);
+            return View();
+        }
+    }
+}
---
+++ Gorman.API.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gorman.API.Web.MvcApplication" Language="C#" %>
