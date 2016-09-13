0341da635cf2162a9ec184c7534ba5d2c2ae9a51
AdamJamesNaylor <adamnaylor@gmail.com>
2016-09-13 09:11:14 +0100

    Stuff

--- Gorman.API.Web/Controllers/HomeController.cs
+++ Gorman.API.Web/Controllers/HomeController.cs
@@ -1,18 +1,18 @@
 ﻿
 namespace Gorman.API.Web.Controllers
 {
+    using System;
     using System.Threading.Tasks;
     using System.Web.Mvc;
     using Framework;
     using Framework.Services;
-    using Framework.Validators;
 
     public class HomeController : Controller
     {
-        // GET: Home
         public async Task<ActionResult> Index() {
-            var endpoints = await Endpoints.Get("http://localhost:9999", new ResponseValidator());
+            var endpoints = await Endpoints.Get(new Uri("http://localhost:6947"));
             var service = new FullGraphService(endpoints);
+            var map = await service.Get(123);
             return View();
         }
     }
