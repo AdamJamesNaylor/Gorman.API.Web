e4329f3d926856d4e469700adbebfd1d2a277269
Adam Naylor <adamnaylor@gmail.com>
2016-09-16 17:28:20 +0100

    Fixing map binding

--- Gorman.API.Web/Controllers/HomeController.cs
+++ Gorman.API.Web/Controllers/HomeController.cs
@@ -5,14 +5,17 @@ namespace Gorman.API.Web.Controllers
     using System.Threading.Tasks;
     using System.Web.Mvc;
     using Framework;
+    using Framework.Domain;
     using Framework.Services;
 
     public class HomeController : Controller
     {
         public async Task<ActionResult> Index() {
             var endpoints = await Endpoints.Get(new Uri("http://localhost:6947"));
-            var service = new FullGraphService(endpoints);
-            var map = await service.Get(123);
+            var graphService = new FullGraphService(endpoints);
+            var mapService = new MapService(endpoints);
+            var map = await mapService.Add(new Map { TileUrl = "blargh" });
+            map = await graphService.Get(map.Id);
             return View();
         }
     }
