6293dab2700a0b12940c62d5724dcaf9bf9d16c4
AdamJamesNaylor <adamnaylor@gmail.com>
2017-03-05 23:47:00 +0000

    blah

--- Gorman.API.Web/Controllers/HomeController.cs
+++ Gorman.API.Web/Controllers/HomeController.cs
@@ -10,12 +10,24 @@ namespace Gorman.API.Web.Controllers
 
     public class HomeController : Controller
     {
+        //todo rework errors as follows http://stackoverflow.com/a/1081853/17540
         public async Task<ActionResult> Index() {
             var endpoints = await Endpoints.Get(new Uri("http://localhost:6947"));
-            var graphService = new FullGraphService(endpoints);
+            var activityService = new ActivityService(endpoints);
             var mapService = new MapService(endpoints);
-            var map = await mapService.Add(new Map { TileUrl = "blargh" });
-            map = await graphService.Get(map.Id);
+
+            var portOlisarInteriorMap = new Map { TileUrl = "blargh" };
+            portOlisarInteriorMap = await mapService.Add(portOlisarInteriorMap);
+            var rootStrategy = new Activity { MapId = portOlisarInteriorMap.Id };
+            rootStrategy = await activityService.Add(rootStrategy);
+            var moveIntoPositionActivity = new Activity { ParentId = rootStrategy.Id };
+            moveIntoPositionActivity = await activityService.Add(moveIntoPositionActivity);
+            var sniper = new Actor();
+            var grunt = new Actor();
+            rootStrategy.
+            var rootActivity = await activityService.Get(moveIntoPositionActivity.Id);
+            /*var graphService = new FullGraphService(endpoints);
+            map = await graphService.Get(map.Id);*/
             return View();
         }
     }
