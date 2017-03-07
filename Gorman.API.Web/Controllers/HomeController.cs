de07cb2ca1f2358f532bc5bbd3e4514350643b91
Adam Naylor <adamnaylor@gmail.com>
2017-03-07 17:13:23 +0000

    Split calls to api

--- Gorman.API.Web/Controllers/HomeController.cs
+++ Gorman.API.Web/Controllers/HomeController.cs
@@ -8,6 +8,11 @@ namespace Gorman.API.Web.Controllers {
     using Framework.Domain;
     using Framework.Services;
 
+    //http://tacnet.io/tacsketch/#
+    //http://www.stratuga.com/editor/
+    //http://www.csgoboard.com/board
+    //https://katanaapp.com
+
     public class HomeController : Controller {
         //todo rework errors as follows http://stackoverflow.com/a/1081853/17540
         public async Task<ActionResult> Index() {
@@ -18,24 +23,25 @@ namespace Gorman.API.Web.Controllers {
             var portOlisarInteriorMap = new Map {TileUrl = "blargh"};
             portOlisarInteriorMap = await mapService.Add(portOlisarInteriorMap);
 
-            var sniper = new Actor();
+            var sniper = new Actor { PositionX = 100 };
             var grunt = new Actor();
             var rootStrategy = new Activity {
                 MapId = portOlisarInteriorMap.Id,
                 Actors = new Collection<Actor> {sniper, grunt}
             };
-            rootStrategy = await activityService.Add(rootStrategy);
-            return View();
 
-            var moveIntoPositionActivity = new Activity {ParentId = rootStrategy.Id};
-            moveIntoPositionActivity = await activityService.Add(moveIntoPositionActivity);
+            var moveIntoPositionActivity = new Activity { ParentId = rootStrategy.Id };
+            var moveSniper = new Framework.Domain.Action {
+                ActorId = rootStrategy.Actors[0].Id,
+                ActivityId = moveIntoPositionActivity.Id,
+            };
+            moveIntoPositionActivity.Actions.Add(moveSniper);
+            rootStrategy.Activities.Add(moveIntoPositionActivity);
+            rootStrategy = await activityService.Add(rootStrategy);
 
             //https://github.com/ewoken/Leaflet.MovingMarker
-            var moveSniper = new Framework.Domain.Action {
-                ActorId = moveIntoPositionActivity.Actors[0].Id,
-                ActivityId = moveIntoPositionActivity.Id,
-                //Type = ActionType.
-            };
+            moveSniper.Parameters.Add(ActionParameter.PositionX, "200");
+            
             var rootActivity = await activityService.Get(moveIntoPositionActivity.Id);
             return View();
         }
